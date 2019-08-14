2ea27e0376b63bdcee5ead97fa1187d3436d4c71
Sunghyun Kim <sunghyun_k@icloud.com>
2019-08-14 22:26:14 +0900

    AVCapture 작성

--- Opener/ViewController.swift
+++ Opener/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
+import os.log
 
 class ViewController: UIViewController {
+    
+    //MARK: AV 프로퍼티
+    
+    var session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var capturePhotoOutput = AVCapturePhotoOutput()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupAVCapture()
+        startCaptureSession()
     }
 
+    func setupAVCapture() {
+        var deviceInput: AVCaptureDeviceInput
+        
+        session.sessionPreset = .vga640x480
+        
+        guard let frontCamera = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else {
+            os_log("전면 카메라에 접근할 수 없음", log: OSLog.default, type: .error)
+            return
+        }
+        
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: frontCamera)
+        } catch {
+            os_log("비디오 입력 장치를 생성할 수 없음", log: OSLog.default, type: .error)
+            return
+        }
+        
+        guard session.canAddInput(deviceInput) else {
+            os_log("입력 장치를 세션에 추가할 수 없음", log: OSLog.default, type: .error)
+            session.commitConfiguration()
+            return
+        }
+        session.addInput(deviceInput)
+        
+        guard session.canAddOutput(capturePhotoOutput) else {
+            os_log("캡쳐 사진 출력을 세션에 추가할 수 없음", log: OSLog.default, type: .error)
+            session.commitConfiguration()
+            return
+        }
+        session.addOutput(capturePhotoOutput)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.bounds
+        view.layer.addSublayer(previewLayer)
+    }
+    
+    func startCaptureSession() {
+        session.startRunning()
+    }
 
 }
 
