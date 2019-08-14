3e2bcb42ff7965916f5d21044211a7694e0a6bb9
Sunghyun Kim <sunghyun_k@icloud.com>
2019-08-15 03:58:18 +0900

    멤버 구조체를 생성

---
+++ Opener/Member.swift
@@ -0,0 +1,14 @@
+//
+//  Member.swift
+//  Opener
+//
+//  Created by 김성현 on 15/08/2019.
+//  Copyright © 2019 Sunghyun Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Member: Codable {
+    var name: String
+    var isEntry: Bool
+}
